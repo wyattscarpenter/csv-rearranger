2cc3db1b9a5613bddebde60df76912dd0babc9cb
wyattscarpenter <wyattscarpenter@gmail.com>
2018-07-16 23:48:34 -0700

    clean up code

--- rearranger.py
+++ rearranger.py
@@ -1,68 +1,57 @@
 import string
 
-DEBUG = True
+DEBUG = False
 def dprint(obj):
     global DEBUG
     if DEBUG:
         print(obj)
         
 def col2num(col):
-    # this function by Sylvain on stack overflow
+    # this function by Sylvain on stack overflow, thanks Slyvain!
     # https://stackoverflow.com/a/12640614
-    # thanks Slyvain!
     num = 0
     for c in col:
         if c in string.ascii_letters:
             num = num * 26 + (ord(c.upper()) - ord('A')) + 1
     return num
 
-def num2col(n):
-    # this function was written by sundar nataraj and MinChul on stack overflow
-    # https://stackoverflow.com/a/23862195
-    # thanks guys!
-    string = ""
-    while n > 0:
-        n, remainder = divmod(n - 1, 26)
-        string = chr(65 + remainder) + string
-    return string
 def id2coord(idstr):
     dprint(idstr)
     num = ""
     col = ""
-    #this is inelegant:
-    try:
-        while True:
-            c = idstr[0]
-            idstr = idstr[1:]
-            if c.isdigit():
-                num+=c
-            else:
-                idstr = c + idstr
-                break
-        if idstr.isalpha():
-            col = idstr
-        dprint(num)
-        dprint(col)
-        if num and col:
-            return (int(num)-1, col2num(col)-1)
+    #this is a bit inelegant:
+    while idstr: # pull out the digits
+        c = idstr[0]
+        idstr = idstr[1:]
+        if c.isdigit():
+            num+=c
         else:
-            return False
-    except:
-        dprint("something went wrong?")
+            idstr = c + idstr
+            break
+    if idstr.isalpha(): #check if the remaining portion is valid for an id
+        col = idstr
+    dprint(num)
+    dprint(col)
+    if num and col:
+        return (int(num)-1, col2num(col)-1)
+    else:
         return False
 
-# 0-indexed internally:
-data = []
+data = [] # 0-indexed internally
 outstr = ""
+
 with open("input.csv") as input:
     text = input.read()
-    for index, line in enumerate(text.splitlines(), start=1):
+    for line in text.splitlines():
         data.append(line.split(","))
 
-with open("transform.csv") as input:
-  text = input.read()
-  for index, line in enumerate(text.splitlines(), start=1):
+with open("transform.csv") as transform:
+  text = transform.read()
+  for line in text.splitlines():
         for item in line.split(","):
+            item = item.strip() # in case the user put in erronious spaces
+            if item == "":
+                break
             coord = id2coord(item)
             if coord:
                 outstr+=str(data[coord[0]][coord[1]])
